acb4f24b931298d9b777491be7f49cfce7ee1cea
Max Khamrovskyi <maksim36ua@gmail.com>
2016-01-06 15:55:55 +0200

    Updated v1.2 of program. Added HTML page parsing, minor refactoring, completed several methods. File input/output, results printed to console

--- CodewarsDigest_v1.2/CodewarsDigest_v1.2/Program.cs
+++ CodewarsDigest_v1.2/CodewarsDigest_v1.2/Program.cs
@@ -13,7 +13,8 @@ namespace CodewarsDigest_v1._2
         public static void ExtractUsersFromHTML(List<UserInfo> userList)
         {     
             HtmlDocument page = new HtmlDocument();
-            page.Load(@"C:\Users\maksi\Documents\GitHub\CodewarsDigest\CodewarsDigest_v1.2\CodewarsDigest_v1.2\Data\1.html");
+            //page.Load(@"C:\Users\maksi\Documents\GitHub\CodewarsDigest\CodewarsDigest_v1.2\CodewarsDigest_v1.2\Data\1.html");
+            page.Load(@"Data\1.html");
             List<HtmlNode> root = page.DocumentNode.Descendants()
                 .Where(n => (n.Name == "div" && n.Attributes["class"] != null && n.Attributes["class"].Value.Contains("leaderboard pan"))).ToList();
 
@@ -33,9 +34,9 @@ namespace CodewarsDigest_v1._2
             
         }
 
-        public static void ExtractUsersFromTXT(List<UserInfo> userList)
+        public static void ExtractUsersFromTXT(List<UserInfo> userList, int lastWeekNumber)
         {
-            FileStream file = new FileStream(@"C:\Users\maksi\Documents\GitHub\CodewarsDigest\CodewarsDigest_v1.2\CodewarsDigest_v1.2\Data\WeeklyRatings", FileMode.Open);
+            FileStream file = new FileStream(@"Data\WeeklyRating\totalRating" + lastWeekNumber + ".txt", FileMode.Open);
             StreamReader stream = new StreamReader(file);
 
             List<string[]> listOfUsers = new List<string[]>();
@@ -46,47 +47,109 @@ namespace CodewarsDigest_v1._2
                 listOfUsers.Add(userInfo);
             }
 
-            foreach (var user in listOfUsers)  // Check from end
-                foreach(var userObject in userList)
-                    if(user[0] == userObject.Name)
-
-
-          }
-
-        public static void ExtractVkLinks()
-        {
-
+            foreach (var user in listOfUsers)  // For each in list from .txt
+                foreach (var userObject in userList) // For each in list of user objects
+                {
+                    if (2 == user.Length) // If username consist of 1 word
+                    {
+                        if (user[0] == userObject.Name)
+                            userObject.LastWeekPoints = int.Parse(user[1]);
+                    }
+                    else if (3 == user.Length)
+                    {
+                        if ((user[0]+" "+user[1]) == userObject.Name)
+                            userObject.LastWeekPoints = int.Parse(user[2]);
+                    }
+                }
+                    
         }
 
-        public static void GenerateWeekRating()
+        public static void GenerateWeekRating(List<UserInfo> userList)
         {
-
+            foreach (var user in userList)
+                user.PointsForThisWeek = user.CurrentPoints - user.LastWeekPoints;
         }
 
-        public static void PrintCurrentPointsToTXT()
+        public static List<string[]> ExtractVkLinksFromTXT()
         {
+            using (FileStream fileNicks = new FileStream(@"Data\nicknames.txt", FileMode.Open))
+            {
+                using (StreamReader streamNicks = new StreamReader(fileNicks))
+                {
+                    List<string[]> listOfNicknames = new List<string[]>();
+                    while (!streamNicks.EndOfStream) // Separating nickname file to user field
+                    {
+                        string[] nickAndLink = streamNicks.ReadLine()
+                            .Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
+                        listOfNicknames.Add(nickAndLink);
+                    }
 
+                    foreach (var nickAndLink in listOfNicknames) // remove "vk.com"
+                    {
+                        var lastIndex = nickAndLink[1].LastIndexOf('/');
+                        if (lastIndex != -1)
+                        {
+                            nickAndLink[1] = nickAndLink[1].Substring(lastIndex + 1,
+                                nickAndLink[1].Length - (lastIndex + 1));
+                        }
+                    }
+
+                    return listOfNicknames;
+                }
+            }
+        }
+
+        public static void PrintCurrentPointsToTXT(List<UserInfo> userList, int thisWeekNumber)
+        {
+            using (FileStream file = new FileStream(@"Data\WeeklyRating\totalRating" + thisWeekNumber + ".txt", FileMode.Append))
+            {
+                using (StreamWriter stream = new StreamWriter(file))
+                {
+                    foreach (var user in userList)
+                        stream.WriteLine(user.Name + " " + user.CurrentPoints);
+                }
+            }
         }
 
         public static void PrintWeekRatingInHTML()
         {
 
-        }
+        } // TODO
 
         public static void PrintTotalRatingInConsole()
         {
+            // Sort users by result
 
-        }
+            List<string[]> listOfNicknamesAndVKLinks = ExtractVkLinksFromTXT();
+            
+
+
+        } // TODO
 
         static void Main(string[] args)
         {
             List<UserInfo> userList = new List<UserInfo>();
+            int thisWeekNumber = 0;
+
+            for (int i = 0; i < 100; i++) // Checking what is the number of current week
+            {
+                if (!File.Exists(@"Data\WeeklyRating\totalRating" + i + ".txt"))
+                {
+                    thisWeekNumber = i;
+                    break;
+                }
+                else continue;
+            }
 
             ExtractUsersFromHTML(userList);
-            ExtractUsersFromTXT(userList);
+            ExtractUsersFromTXT(userList, thisWeekNumber - 1);
+            GenerateWeekRating(userList);
+            PrintCurrentPointsToTXT(userList, thisWeekNumber);
+            PrintTotalRatingInConsole();
 
-            foreach (var user in userList)
-                Console.WriteLine(user.Name + " " + user.CurrentPoints);
+            //foreach (var user in userList)
+            //    Console.WriteLine(user.Name + " " + user.LastWeekPoints 
+            //        + " " + user.CurrentPoints + " : " +user.PointsForThisWeek);
 
             Console.Read();
         }
